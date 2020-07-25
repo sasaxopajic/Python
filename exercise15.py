7b33932d62e4797e5c8e0382c528b55820e307a1
sasaxopajic <sasaxxxpajic@gmail.com>
2020-07-25 13:06:48 +0200

    Exercise 15

--- exercise15.py
+++ exercise15.py
@@ -5,6 +5,6 @@ if call <= 500:
 elif 501 <= call <=800:
     price = 500*0.01 + (call-500)*0.008
 else:
-    price = 500*0.01 + 299*0.008 + (call-800)*0.005
+    price = 500*0.01 + 300*0.008 + (call-800)*0.005
 
 print("Total amount: ", price, "$.")
