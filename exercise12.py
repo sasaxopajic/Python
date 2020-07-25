71fb0b35887248ecfd805b5de74c5384665df95f
sasaxopajic <sasaxxxpajic@gmail.com>
2020-07-25 12:38:06 +0200

    Exercise 12

---
+++ exercise12.py
@@ -0,0 +1,10 @@
+x = int(input("Wikipedia: "))
+y = int(input("Memes: "))
+
+total = x * 0.10 + y * 0.05
+
+if total >= 100:
+    print("Too much consumption!")
+
+if y * 0.05 > x * 0.10:
+    print("WOW MANY MEMES \nSUCH LOL")
