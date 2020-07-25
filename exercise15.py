5ea35c6af4feaaeeb66e16dfa81a657cd4604805
sasaxopajic <sasaxxxpajic@gmail.com>
2020-07-25 13:06:21 +0200

    Exercise 15

---
+++ exercise15.py
@@ -0,0 +1,10 @@
+call = int(input("How many seconds was the call's duration? "))
+
+if call <= 500:
+    price = call*0.01
+elif 501 <= call <=800:
+    price = 500*0.01 + (call-500)*0.008
+else:
+    price = 500*0.01 + 299*0.008 + (call-800)*0.005
+
+print("Total amount: ", price, "$.")
