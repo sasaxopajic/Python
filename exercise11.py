9becea11dac45b28b16e221ee39142a4fc4a0bc4
sasaxopajic <sasaxxxpajic@gmail.com>
2020-07-25 12:10:11 +0200

    Exercise 11

---
+++ exercise11.py
@@ -0,0 +1,6 @@
+hrs = int(input("Hours: "))
+inc = int(input("Income per hour: "))
+
+money = hrs*inc
+
+print("I can buy", int(money/200), "PS4,", int(money/900), "Samsung,", int(money/500), "TV,", int(money/9.99), "game skin.")
